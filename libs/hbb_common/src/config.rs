f592fb865882b011a529004b4a5985a34134a9d5
gangfenglu <lufenggang@126.com>
2024-10-10 18:42:53 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["wangyj.8866.org"];
-pub const PUBLIC_RS_PUB_KEY: &str = "";
+pub const PUBLIC_RS_PUB_KEY: &str = "a123456";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
